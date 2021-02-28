ae75cee9640209bc025d8e6c32763a58b37b4f97
Ann <shatkovskaya_a_v@mail.ru>
2021-02-28 23:08:10 +0300

    Config file was edited

--- conftest.py
+++ conftest.py
@@ -10,6 +10,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
+    user_language = request.config.getoption("language")
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
